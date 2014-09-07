a6fa2792dc59f2cca9c47f3af2c0a01c5653ac09
Yash Aggarwal <yash0609@gmail.com>
2014-09-07 11:12:58 -0500

    get some shitty ui to work

--- js/grade_view_tweaks.js
+++ js/grade_view_tweaks.js
@@ -2,14 +2,27 @@ function date_string_to_unix(string) {
 	return moment(string, "MM/DD/YYYY").unix();
 }
 
-function getDataFromTable(table, header_row) {
-  var rawTable = [];
-  var final_data = [];
-  $(table).find("tr").each(function(index, element) {
+// Take a HTML table like the follows:
+// Name | Grade | Awesomeness (1-10)
+// ----------------------------------
+// Yash | College | 9001
+// Simon | College | 9001
+// John | 2         | 5
+
+// .. And convert it into this:
+// [{"Name": "Yash", "Grade": "College", "Awesomeness": 9001}, ..., ...]
+// Uses first row as the header table, or you can specify in header_row
+function getDataFromTable(table, header_row, exclude_last) {
+  exclude_last = exclude_last || false;
+  var rawTable = []; // array of arrays representing raw table
+  var final_data = []; // array of objects
+  var query = exclude_last ? "tr:not(:last)" : "tr";
+  $(table).find(query).each(function(index, element) {
+    //get text of each element in the row and keep it in an array
     var row = _.map(element.children, function(element) { return element.innerText; });
     rawTable.push(row);
   });
-  var header_row_from_webpage = rawTable[0];
+  var header_row_from_webpage = rawTable[0]; // first row is the header
   header_row = header_row || header_row_from_webpage;
   for(var i = 1; i < rawTable.length; i++) {
     final_data.push(_.object(header_row, rawTable[i]));
@@ -36,7 +49,17 @@ function scrapeData() {
     var category_information_table = course_el_jq.find(".sg-content-grid .sg-asp-table-group .sg-asp-table");
     if(category_information_table.length === 1) {
       course_obj.categories = getDataFromTable(category_information_table,
-        ["category_name", "student_points", "maximum_points", "percent", "weight", "category_points"]);
+        ["category_name", "student_points", "maximum_points", "percent", "weight", "category_points"], true);
+      var assignments_by_category = _.groupBy(course_obj.assignments, function(assignment) { return assignment.category; });
+      course_obj.categories.forEach(function(category, index) {
+        var assignments_under_current_category = assignments_by_category[category.category_name];
+        course_obj.categories[index].assignments = assignments_under_current_category;
+        delete assignments_by_category[category.category_name];
+      });
+      for(var category_name in assignments_by_category) {
+        var assignments_under_current_category = assignments_by_category[category_name];
+        course_obj.categories.push({category_name: category_name, student_points: 0, maximum_points: 0, percent: 0, weight: 0, category_points: 0, assignments: assignments_under_current_category});
+      }
     } else {
       course_obj.categories = [];
     }
@@ -66,9 +89,8 @@ function scrapeData() {
 // 		}
 // 	});
 // });
-
-console.log(getDataFromTable($(".sg-asp-table").get(0)));
-console.log(scrapeData());
+var data = scrapeData();
+//console.log(data);
 
 //gotten from github, removes css styles
 (function() {
@@ -100,4 +122,141 @@ console.log(scrapeData());
     return [].slice.call(document.querySelectorAll(selector));
   }
 })();
-//08/26/2014
+
+var new_ui_div = $("<div id='new_ui' />");
+$("body").prepend(new_ui_div);
+
+// var CourseInfo = React.createClass({
+//   render: function() {
+//     var categories = _.map(this.props.data.categories, function(category) {
+//       return (<CategoryInfo data={category} />);
+//     });
+//     return (
+//       <div class="course_info">
+//         <h1>{this.props.data.name}: {this.props.data.grade}</h1>
+//         {categories}
+//       </div>
+//     );
+//   }
+// });
+var CourseInfo = React.createClass({displayName: 'CourseInfo',
+  render: function() {
+    var categories = _.map(this.props.data.categories, function(category) {
+      return (CategoryInfo({data: category}));
+    });
+    return (
+      React.DOM.div({className: "course_info"},
+        React.DOM.h1(null, this.props.data.name, ": ", this.props.data.grade),
+        categories
+      )
+    );
+  }
+});
+
+// var CategoryInfo = React.createClass({
+//   render: function() {
+//     var tableRows = _.map(this.props.data.assignments, function(assignment) {
+//       return (
+//       <tr>
+//         <td>{assignment.assignment_name}</td>
+//         <td>{assignment.score}/{assignment.total_points}</td>
+//         <td>{assignment.weight}</td>
+//         <td>{assignment.average_score}</td>
+//       </tr>
+//       );
+//     });
+//     return (
+//       <div id="category">
+//         <h2>{this.props.data.category_name}: {this.props.data.weight} </h2>
+//         <table>
+//           <thead>
+//             <tr>
+//               <th>Assignment Name</th>
+//               <th>Score/Total</th>
+//               <th>Weight</th>
+//               <th>Average Score</th>
+//             </tr>
+//           </thead>
+//           <tbody>
+//             {tableRows}
+//           </tbody>
+//           <tfoot>
+//             <tr>
+//               <td>Total:</td>
+//               <td>{this.props.data.percent}</td>
+//               <td></td>
+//               <td></td>
+//             </tr>
+//           </tfoot>
+
+//         </table>
+//       </div>
+//     )
+//   }
+// });
+var CategoryInfo = React.createClass({displayName: 'CategoryInfo',
+  render: function() {
+    var tableRows = _.map(this.props.data.assignments, function(assignment) {
+      return (
+      React.DOM.tr(null,
+        React.DOM.td(null, assignment.assignment_name),
+        React.DOM.td(null, assignment.score, "/", assignment.total_points),
+        React.DOM.td(null, assignment.weight),
+        React.DOM.td(null, assignment.average_score)
+      )
+      );
+    });
+    return (
+      React.DOM.div({id: "category"},
+        React.DOM.h2(null, this.props.data.category_name, ": ", this.props.data.weight, " "),
+        React.DOM.table(null,
+          React.DOM.thead(null,
+            React.DOM.tr(null,
+              React.DOM.th(null, "Assignment Name"),
+              React.DOM.th(null, "Score/Total"),
+              React.DOM.th(null, "Weight"),
+              React.DOM.th(null, "Average Score")
+            )
+          ),
+          React.DOM.tbody(null,
+            tableRows
+          ),
+          React.DOM.tfoot(null,
+            React.DOM.tr(null,
+              React.DOM.td(null, "Total:"),
+              React.DOM.td(null, this.props.data.percent),
+              React.DOM.td(null),
+              React.DOM.td(null)
+            )
+          )
+
+        )
+      )
+    );
+  }
+});
+
+// var StudentInfo = React.createClass({
+//   render: function() {
+//     var courses = _.map(this.props.data, function(course) {
+//       return (<CourseInfo data={course} />);
+//     });
+//     return (<div class="course">{courses}</div>);
+//   }
+// });
+var StudentInfo = React.createClass({displayName: 'StudentInfo',
+  render: function() {
+    var courses = _.map(this.props.data, function(course) {
+      return (CourseInfo({data: course}));
+    });
+    return (React.DOM.div({className: "courses"}, courses));
+  }
+});
+// React.renderComponent(
+//   <StudentInfo data={data} />,
+//   document.getElementById('new_ui')
+// );
+React.renderComponent(
+  StudentInfo({data: data}),
+  document.getElementById('new_ui')
+);
